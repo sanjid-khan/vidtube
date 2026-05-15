4a492cf3ab0b483811d83171ee4c90f0647f484a
Sanjid Khan <sksanjid295@gmail.com>
2026-05-16 03:49:23 +0600

    feat: configure express app and database connection

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+import {app} from "./app.js"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: "./.env"
+})
+
+const PORT=process.env.PORT || 8001
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(` Server is running at port : ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
